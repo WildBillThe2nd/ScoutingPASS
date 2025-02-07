683188630fd358f8fb25c2322f7285f72fd5f12a
WildBillThe2nd <34014166+WildBillThe2nd@users.noreply.github.com>
2025-02-07 09:03:22 -0500

    Update reefscape_config.js
    Changed Titles

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -1,8 +1,8 @@
 var config_data = `
 {
   "dataFormat": "tsv",
-  "title": "Scouting PASS 2025",
-  "page_title": "REEFSCAPE",
+  "title": "Match Scouting 2025",
+  "page_title": "REEFSCAPE 2025<br>Match Scouting",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
